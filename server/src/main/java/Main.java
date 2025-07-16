41b03439cac579a950144dd791e97bb478f803eb
Joe Guymon <jg737@byu.edu>
2025-07-15 20:35:54 -0600

    CP3: added starter code and set things up

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import chess.*;
+import org.eclipse.jetty.server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        new Server.run();
     }
 }
