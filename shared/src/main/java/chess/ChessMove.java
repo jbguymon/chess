60632f41c67f4ecf30f2aa6d82a675cec3b079b1
Joe Guymon <jg737@byu.edu>
2026-01-21 19:38:41 -0700

    Implemented ChessPosition with Overrides as well as added a toString Override in ChessMove

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -58,4 +58,9 @@ public class ChessMove {
     public int hashCode() {
         return Objects.hash(startPosition, endPosition, promotionPiece);
     }
+
+    @Override
+    public String toString() {
+        return String.format("%s%s", startPosition, endPosition);
+    }
 }
