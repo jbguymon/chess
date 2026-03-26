aaad1a39c75ef0a874c91fdeceb89def96e3be0d
Joe Guymon <jg737@byu.edu>
2026-03-25 23:47:53 -0600

    User Handler functionality

---
+++ server/src/main/java/handler/UserHandler.java
@@ -0,0 +1,30 @@
+package handler;
+import org.eclipse.jetty.server.Authentication;
+import service.UserService;
+import service.RegisterRequest;
+import service.RegisterResult;
+import io.javalin.http.Context;
+import com.google.gson.Gson;
+
+
+public class UserHandler {
+    private final UserService service;
+    private final Gson gson = new Gson();
+
+    public UserHandler(UserService service){
+        this.service = service;
+    }
+
+    public void register(Context context){
+        try{
+            RegisterRequest request = gson.fromJson(context.body(), RegisterRequest.class);
+            RegisterResult result = service.register(request);
+            context.status(200);
+            context.json(result);
+        } catch (Exception exception){
+            String message = exception.getMessage() != null ? exception.getMessage() : "unknown error";
+            context.status(message.equals("Username already taken") ? 403 : 400);
+            context.json("{\"message\":\"Error: " + message + "\"}");
+        }
+    }
+}
