8ebfb7ac620aba7eba4aa525070ddd0555e93177
Joe Guymon <jg737@byu.edu>
2026-03-25 21:57:24 -0600

    data access exception folder

--- shared/src/main/java/model.java
+++
@@ -1,8 +0,0 @@
-import chess.ChessGame;
-
-public class model {
-    public record UserData(String username, String password, String email){}
-    public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game){}
-    public record AuthData(String authToken, String username){}
-}
-
