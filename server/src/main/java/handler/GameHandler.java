3d7b136f3609992cef8c1be8961986c4378f348f
Joe Guymon <jg737@byu.edu>
2026-03-26 23:54:05 -0600

    Game Handler file

---
+++ server/src/main/java/handler/GameHandler.java
@@ -0,0 +1,72 @@
+package handler;
+import io.javalin.http.Context;
+import com.google.gson.Gson;
+import service.*;
+
+public class GameHandler {
+    private final GameService service;
+    private final Gson gson = new Gson();
+
+    public GameHandler(GameService service){
+        this.service = service;
+    }
+
+    public void createGame(Context context){
+        try{
+            String authToken = context.header("authorization");
+            CreateGameRequest request = gson.fromJson(context.body(), CreateGameRequest.class);
+            CreateGameResponse response = service.createGame(authToken, request);
+            context.status(200);
+            context.result(gson.toJson(response));
+        } catch (Exception exception){
+            String message = exception.getMessage();
+            int errorStatus = switch (message){
+                case "bad request" -> 400;
+                case "unauthorized" -> 401;
+                case "already taken" -> 403;
+                default -> 500;
+            };
+            context.status(errorStatus);
+            context.result("{\"message\":\"Error: " + message + "\"}");
+        }
+    }
+
+    public void listGames(Context context){
+        try{
+            String authToken = context.header("authorization");
+            ListGamesResponse response = service.listGames(authToken);
+            context.status(200);
+            context.result(gson.toJson(response));
+        } catch (Exception exception){
+            String message = exception.getMessage();
+            int errorStatus = switch (message){
+                case "bad request" -> 400;
+                case "unauthorized" -> 401;
+                case "already taken" -> 403;
+                default -> 500;
+            };
+            context.status(errorStatus);
+            context.result("{\"message\":\"Error: " + message + "\"}");
+        }
+    }
+
+    public void joinGame(Context context){
+        try {
+            String authToken = context.header("authorization");
+            JoinGameRequest request = gson.fromJson(context.body(), JoinGameRequest.class);
+            service.joinGame(authToken, request);
+            context.status(200);
+            context.result("{}");
+        } catch (Exception exception){
+            String message = exception.getMessage();
+            int errorStatus = switch (message){
+                case "bad request" -> 400;
+                case "unauthorized" -> 401;
+                case "already taken" -> 403;
+                default -> 500;
+            };
+            context.status(errorStatus);
+            context.result("{\"message\":\"Error: " + message + "\"}");
+        }
+    }
+}
