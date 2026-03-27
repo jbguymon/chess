4565ba0d618a32f21205eb60210e23d0fe29456e
Joe Guymon <jg737@byu.edu>
2026-03-26 23:44:35 -0600

    Game Service file

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,58 @@
+package service;
+import dataaccess.dataaccess;
+import model.*;
+import java.util.List;
+
+public class GameService {
+    private final dataaccess data;
+
+    public GameService(dataaccess data){
+        this.data = data;
+    }
+
+    public CreateGameResponse createGame(String authToken, CreateGameRequest request) throws Exception {
+        if(data.getAuth(authToken) == null || authToken == null){
+            throw new Exception("unauthorized");
+        }
+        if(request.gameName() == null){
+            throw new Exception("bad request");
+        }
+        int gameID = data.createGame(request.gameName());
+        return new CreateGameResponse(gameID);
+    }
+
+    public ListGamesResponse listGames(String authToken) throws Exception {
+        if(data.getAuth(authToken) == null || authToken == null){
+            throw new Exception("unauthorized");
+        }
+        List<GameData> gameList = data.listGames();
+        return new ListGamesResponse(gameList);
+    }
+
+    public void joinGame(String authToken, JoinGameRequest request) throws Exception {
+        if(data.getAuth(authToken) == null){
+            throw new Exception("unauthorized");
+        }
+        GameData game = data.getGame(request.gameID());
+        if(game == null){
+            throw new Exception("bad request");
+        }
+        String username = data.getAuth(authToken).username();
+        if(request.color().equals("WHITE")){
+            if(game.whiteUsername() != null){
+                throw new Exception("already taken");
+            }
+            game = new GameData(game.gameID(), username, game.blackUsername(), game.gameName(), game.game());
+        }
+        else if(request.color().equals("BLACK")){
+            if(game.blackUsername() != null){
+                throw new Exception("already taken");
+            }
+            game = new GameData(game.gameID(), username, game.whiteUsername(), game.gameName(), game.game());
+        }
+        else{
+            throw new Exception("bad request");
+        }
+        data.updateGame(game);
+    }
+}
