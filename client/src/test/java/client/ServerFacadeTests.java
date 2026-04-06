9679ed82984df76c6bc1e2c470a798011b208910
Joe Guymon <jg737@byu.edu>
2026-04-05 23:39:05 -0600

    Lab 5 Server Facade register tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,26 @@
 package client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import model.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private ServerFacade facade;
+    private static int port;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -20,10 +29,33 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    private void clearData(){
+        try{
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:" + port + "/db"))
+                    .DELETE()
+                    .build();
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (Exception ignored){
+        }
+    }
+
+    //register tests
+    @Test
+    void registerPositive() throws ResponseException {
+        AuthData auth = facade.register("joe", "password123", "joe@joe.com");
+        assertNotNull(auth);
+        assertNotNull(auth.authToken());
+        assertEquals("joe", auth.username());
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerNegative() throws ResponseException{
+        facade.register("dupe", "password", "dupe@dupe.com");
+        assertThrows(ResponseException.class, () ->
+                facade.register("dupe", "password", "dupe@dupe.com"));
     }
 
 }
