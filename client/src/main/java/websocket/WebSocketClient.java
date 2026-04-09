2ece9633f94777a0533985e7939da6672b8faf97
Joe Guymon <jg737@byu.edu>
2026-04-08 21:11:07 -0600

    Lab 6 Websocket base code

---
+++ client/src/main/java/websocket/WebSocketClient.java
@@ -0,0 +1,34 @@
+package websocket;
+
+import jakarta.websocket.*;
+import java.net.URI;
+
+@ClientEndpoint
+public class WebSocketClient {
+    private Session session;
+
+    public WebSocketClient(String url){
+        try{
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, URI.create(url));
+        }
+        catch(Exception exception){
+            throw new RuntimeException("Failed to connect to server", exception);
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session session){
+        this.session = session;
+        System.out.println("Connected to server");
+    }
+
+    @OnMessage
+    public void onMessage(String message){
+
+    }
+
+    public void sendMessage(String message){
+
+    }
+}
