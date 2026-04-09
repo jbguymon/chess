8bb16de2656947bdb4fe6cbf9f78857f4f5e003e
Joe Guymon <jg737@byu.edu>
2026-04-08 21:32:18 -0600

    Lab 6 WebSocketClient and NotificationHandler

--- client/src/main/java/client/WebSocketClient.java
+++ client/src/main/java/client/WebSocketClient.java
@@ -1,13 +1,18 @@
 package client;
 
+import com.google.gson.Gson;
 import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
+
 import java.net.URI;
 
 @ClientEndpoint
 public class WebSocketClient {
     private Session session;
+    private final NotificationHandler handler;
 
-    public WebSocketClient(String url){
+    public WebSocketClient(String url, NotificationHandler handler){
+        this.handler = handler;
         try{
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             container.connectToServer(this, URI.create(url));
@@ -25,10 +30,31 @@ public class WebSocketClient {
 
     @OnMessage
     public void onMessage(String message){
-
+        handler.notify(message);
     }
 
     public void sendMessage(String message){
+        try{
+            session.getBasicRemote().sendText(message);
+        }
+        catch(Exception exception){
+            throw new RuntimeException(exception);
+        }
+    }
 
+    public void close(){
+        if(session != null && session.isOpen()){
+            try{
+                session.close();
+            }
+            catch(Exception exception){
+            }
+        }
+    }
+
+    public void sendCommand(UserGameCommand command){
+        Gson gson = new Gson();
+        String json = gson.toJson(command);
+        sendMessage(json);
     }
 }
